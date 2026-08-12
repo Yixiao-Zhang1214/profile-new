9b8be7cd61e22e416a93a5af12a8d883d3ba2c70
Yixiao Zhang <172938960@qq.com>
2026-08-12 16:30:42 +0800

    Fix optional asset binding type check

--- worker/index.ts
+++ worker/index.ts
@@ -43,9 +43,9 @@ const worker = {
 
       const allowedWidths = [...DEFAULT_DEVICE_SIZES, ...DEFAULT_IMAGE_SIZES];
       return handleImageOptimization(request, {
-        fetchAsset: (path) => env.ASSETS.fetch(new Request(new URL(path, request.url))),
+        fetchAsset: (path) => env.ASSETS!.fetch(new Request(new URL(path, request.url))),
         transformImage: async (body, { width, format, quality }) => {
-          const result = await env.IMAGES.input(body).transform(width > 0 ? { width } : {}).output({ format, quality });
+          const result = await env.IMAGES!.input(body).transform(width > 0 ? { width } : {}).output({ format, quality });
           return result.response();
         },
       }, allowedWidths);
